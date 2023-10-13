f22db942279cd67ceabe48ad4143f6e455305c85
Lucas Martins <lucasdallmartims@gmail.com>
2023-10-13 18:14:56 -0300

    fim

--- ConectaVagas/src/pages/Home/Home.jsx
+++ ConectaVagas/src/pages/Home/Home.jsx
@@ -15,23 +15,15 @@ export default function Home() {
   const { handleFilter, setEmpty, setFilter, empty, filter } =
     useContext(CompaniesContext);
 
-  /*let jobs = useMemo(() => {
-    const apply = [];
-    for (let i = 0; i < applyList.length; i++) {
-      const verify = vacanciesList.filter(
-        (vacancy) => vacancy.ID == applyList[i].ID
-      );
+  const formatDate = (date) => {
+    const createDate = new Date(date);
+    const format = { year: "numeric", month: "2-digit", day: "2-digit" };
 
-      if (verify.length > 0) {
-        apply.push(verify[0]);
-      }
-    }
-    console.log(apply);
-    console.log(applyList);
-    console.log(vacanciesList);
-    return apply;
-  }, [applyList]);
-*/
+    const formatedDate = createDate.toLocaleDateString("pt-BR", format);
+
+    return formatedDate;
+  };
+
   useEffect(() => {
     vacancyLoadApplications();
   }, []);
@@ -44,22 +36,22 @@ export default function Home() {
           {userData.company ? (
             <>
               <Link to="/jobregister">
-                <span className="text-3xl font-serif font-bold self-center   ">
+                <span className="text-3xl font-sans font-bold self-center   ">
                   Cadastrar Vaga
                 </span>
               </Link>
-              <h1 className="text-3xl font-serif font-bold self-center pt-5">
+              <h1 className="text-3xl font-sans font-bold self-center pt-5">
                 Vagas Criadas:
               </h1>
             </>
           ) : (
-            <h1 className="text-3xl font-serif font-bold self-center pt-10">
+            <h1 className="text-3xl font-sans font-bold self-center pt-10">
               Oportunidades de Vagas:
             </h1>
           )}
         </div>
 
-        <div className="pt-2 grid grid-cols-3">
+        <div className="pt-2 grid grid-cols-3 w-2/3">
           {empty === true ? (
             <p>Nenhuma vaga dessa categoria</p>
           ) : filter.length > 0 ? (
@@ -69,12 +61,29 @@ export default function Home() {
                   <h1 className="flex justify-center pb-2 font-sans text-white text-xl bg-blue-600 p-3 mb-2 rounded-xl shadow-md shadow-black">
                     {vacancy.title}
                   </h1>
-                  <div className="bg-white rounded-xl text-base font-sans px-9 py-4 shadow-sm shadow-black ">
-                    <p> {vacancy.description}</p>
-                    <p> Cidade: {vacancy.location}</p>
-                    <p> Categorias: {vacancy.filters}</p>
-                    <p> Salário: {vacancy.salary}</p>
-                    <p>Data de publicação: {vacancy.postDate}</p>
+                  <div className="bg-white rounded-xl text-base font-sans font-bold px-9 py-4 shadow-sm shadow-black">
+                    <p className="px-6"> {vacancy.description}</p>
+                    <div className="pb-2 pt-2">
+                      <BsFillGeoAltFill size={18} className="absolute" />
+                      <p className="px-6">{vacancy.location}</p>
+                    </div>
+                    <div className="pb-2">
+                      <BsStack size={18} className="absolute" />
+                      <p className="px-6">{vacancy.filters}</p>
+                    </div>
+                    <div className="pb-2">
+                      <MdOutlineAttachMoney size={20} className="absolute" />
+                      <p className="px-6">
+                        {vacancy.salary.toLocaleString("pt-BR", {
+                          style: "currency",
+                          currency: "BRL",
+                        })}
+                      </p>
+                    </div>
+                    <div>
+                      <BsCalendarDate size={18} className="absolute" />
+                      <p className="px-6">{formatDate(vacancy.postDate)}</p>
+                    </div>
                   </div>
                 </Link>
               </div>
@@ -86,12 +95,29 @@ export default function Home() {
                   <h1 className="flex justify-center pb-2 font-sans text-white text-xl bg-blue-600 p-3 mb-2 rounded-xl shadow-md shadow-black">
                     {vacancy.title}
                   </h1>
-                  <div className="bg-white rounded-xl text-base font-serif font-bold px-9 py-4 shadow-sm shadow-black ">
-                    <p> {vacancy.description}</p>
-                    <p> Cidade: {vacancy.location}</p>
-                    <p> Categorias: {vacancy.filters}</p>
-                    <p> Salário: {vacancy.salary}</p>
-                    <p>Data de publicação: {vacancy.postDate}</p>
+                  <div className="bg-white rounded-xl text-base font-sans font-bold px-9 py-4 shadow-sm shadow-black">
+                    <p className="px-6"> {vacancy.description}</p>
+                    <div className="pb-2 pt-2">
+                      <BsFillGeoAltFill size={18} className="absolute" />
+                      <p className="px-6">{vacancy.location}</p>
+                    </div>
+                    <div className="pb-2">
+                      <BsStack size={18} className="absolute" />
+                      <p className="px-6">{vacancy.filters}</p>
+                    </div>
+                    <div className="pb-2">
+                      <MdOutlineAttachMoney size={20} className="absolute" />
+                      <p className="px-6">
+                        {vacancy.salary.toLocaleString("pt-BR", {
+                          style: "currency",
+                          currency: "BRL",
+                        })}
+                      </p>
+                    </div>
+                    <div>
+                      <BsCalendarDate size={18} className="absolute" />
+                      <p className="px-6">{formatDate(vacancy.postDate)}</p>
+                    </div>
                   </div>
                 </Link>
               </div>
@@ -105,19 +131,36 @@ export default function Home() {
             Vagas Aplicadas:
           </h1>
         )}
-        <div className="pt-2 grid grid-cols-3 ">
+        <div className="pt-2 grid grid-cols-3 w-2/3 ">
           {applyList.map((vacancy, index) => (
             <div className="bg-azul-100 pt-5 flex flex-col pr-3" key={index}>
               <Link to={`/vacancieDetails/${vacancy.ID}`}>
                 <h1 className="flex justify-center pb-2 font-sans text-white text-xl bg-blue-600 rounded-xl shadow-md shadow-black p-3 mb-2">
                   {vacancy.title}
                 </h1>
-                <div className="bg-white rounded-xl text-base font-serif font-bold px-9 py-4 shadow-sm shadow-black">
-                  <p> {vacancy.description}</p>
-                  <p> Cidade: {vacancy.location}</p>
-                  <p> Categorias: {vacancy.filters}</p>
-                  <p> Salário: {vacancy.salary}</p>
-                  <p>Data de publicação: {vacancy.postDate}</p>
+                <div className="bg-white rounded-xl text-base font-sans font-bold px-9 py-4 shadow-sm shadow-black">
+                  <p className="px-6"> {vacancy.description}</p>
+                  <div className="pb-2 pt-2">
+                    <BsFillGeoAltFill size={18} className="absolute" />
+                    <p className="px-6">{vacancy.location}</p>
+                  </div>
+                  <div className="pb-2">
+                    <BsStack size={18} className="absolute" />
+                    <p className="px-6">{vacancy.filters}</p>
+                  </div>
+                  <div className="pb-2">
+                    <MdOutlineAttachMoney size={20} className="absolute" />
+                    <p className="px-6">
+                      {vacancy.salary.toLocaleString("pt-BR", {
+                        style: "currency",
+                        currency: "BRL",
+                      })}
+                    </p>
+                  </div>
+                  <div>
+                    <BsCalendarDate size={18} className="absolute" />
+                    <p className="px-6">{formatDate(vacancy.postDate)}</p>
+                  </div>
                 </div>
               </Link>
             </div>
