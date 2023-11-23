1a19f4834883dcf2684b2ced0c4671cdb70aff56
Lucas Martins <lucasdallmartims@gmail.com>
2023-11-23 20:48:32 -0300

    Funcionaaaaaaaa

--- ConectaVagas/src/Axios/Api.js
+++ ConectaVagas/src/Axios/Api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "conectavagas-production.up.railway.app",
+  baseURL: "https://conectavagas-production.up.railway.app",
 });
